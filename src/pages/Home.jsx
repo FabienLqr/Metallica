f2d05c9bef246f404a53cfe4b97aaba2af9a0f8c
Fabien LAQUERRIERE <fabienlqr@gmail.com>
2024-07-08 11:54:37 +0200

    set carte de membre pour Hetfield

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,7 @@
+/* eslint-disable react/no-unescaped-entities */
 // import datas
 import Metallica from "../assets/Images/MetallicaLogo.jpg";
+import James from "../assets/Images/James.jpg";
 
 function Home() {
   return (
@@ -18,13 +20,35 @@ function Home() {
         </article>
         <h2>Les membres</h2>
         <article className="homeArticles">
+          <div className="card-container">
+            <div className="img-container">
+              <img src={James} className="card-img" alt="James Hetfield" />
+            </div>
+            <section>
+              <h3>James Hetfield</h3>
+              <h4>Chanteur rythmique</h4>
+              <article className="grp-description">
+                <p>James Hetfield est né le 3 août 1963</p>
+                <blockquote className="james-quote">
+                  "I was pretty much afraid of everything. Afraid of the world,
+                  afraid of speaking. You know really shy kid, music is a way to
+                  speak, as simple as that. I could either just Journal write
+                  poetry, write whatever was in my head and sit down drum a few
+                  chords and put that together and that's me, that's me telling
+                  the world about me when I can't do it on my own so music was
+                  the voice I didn't have."
+                </blockquote>
+              </article>
+            </section>
+          </div>
           <ul>
-            <li>James HETFIELD: Guitariste et chanteur</li>
             <li>Kirk HAMMET: guitariste lead</li>
             <li>Robert TRUJILLO: Bassiste</li>
             <li>Lars ULRICH: Batteur</li>
           </ul>
-          <h3>Anciens Membres</h3>
+        </article>
+        <h2>Anciens Membres</h2>
+        <article className="homeArticles">
           <ul>
             <li>
               Dave MUSTAIN qui a ete exclus, il a ensuite forme le groupe
