195230f935a170cd66cee9e4eba73c794c22aac3
Fabien LAQUERRIERE <fabienlqr@gmail.com>
2024-07-11 10:55:59 +0200

    je continue la home page

---
+++ src/components/CardAncien.jsx
@@ -0,0 +1,63 @@
+/* eslint-disable react/no-unescaped-entities */
+import { useState } from "react";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+// import img
+import Cliff from "../assets/Images/Cliff.jpg";
+import Dave from "../assets/Images/Dave.jpg";
+import Jason from "../assets/Images/Jason.jpg";
+
+function AncienMembre() {
+  const [currentCardIndex] = useState(0);
+  const cards = [
+    {
+      img: Cliff,
+      name: "Cliff Burton",
+      role: "Bassiste",
+      description: "Cliff Burton est né le 10 février 1962",
+      note: "Cliff Burton etait le bassiste de Metallica. IL est mort lors d'un accident avec le bus de la tournee le 27 septembre 1986.",
+    },
+    {
+      img: Dave,
+      name: "Dave Mustaine",
+      role: "Guitare lead",
+      description: "Dave Mustaine est né le 13 septembre 1961",
+      note: "Il fut exclus du groupe en 1983, il fonda par la suite le groupe Megadeth",
+    },
+    {
+      img: Jason,
+      name: "Jason Newsted",
+      role: "Bassiste",
+      description: "Jason NEWSTED est né le 4 mars 1963",
+      note: "Il prit la suite de Burton a la basse de 1986 jusqu'au 2001 ou il quitta le groupe",
+    },
+  ];
+  return (
+    <>
+      <Carousel>
+        {cards.map((card, index) => (
+          <>
+            <div
+              className={`card-container ${
+                index === currentCardIndex ? "active" : ""
+              }`}
+              key={index}
+            >
+              <img src={card.img} className="card-img" alt={card.name} />
+              <section>
+                <h3>{card.name}</h3>
+                <p className="role">{card.role}</p>
+                <article className="grp-description">
+                  <p>{card.description}</p>
+                  <blocknote className="note">"{card.note}"</blocknote>
+                </article>
+              </section>
+            </div>
+          </>
+        ))}
+      </Carousel>
+    </>
+  );
+}
+
+export default AncienMembre;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/no-unescaped-entities */
 import CardGroupe from "../components/CardGroupe";
+import CardAncien from "../components/CardAncien";
 // import datas
 import Metallica from "../assets/Images/MetallicaLogo.jpg";
 
@@ -10,14 +11,32 @@ function Home() {
       <section>
         <h2>Qui sont-ils?</h2>
         <article className="homeArticles">
-          Metallica est un groupe de trash metal et heavy metal originaire de
-          Califonie (Etats-Unis d&apos;Amerique). La naissance du groupe est du
-          a une annonce postée dans le journal <em>The Recycler</em> de Lars
-          ULRICH pour former un groupe en 1980, c&apos;est James HETFIELD qui a
-          repondu. Le groupe c&apos;est forme a Los Angeles en 1981. Metallica a
-          eu d&apos;autres membres avant ceux que l&apos;on connait
-          actuellement, c&apos;est principalement la place de bassiste qui a le
-          plus change
+          <p>
+            Metallica est un groupe de trash metal et heavy metal originaire de
+            Califonie (Etats-Unis d&apos;Amerique). La naissance du groupe est
+            du a une annonce postée dans le journal <em>The Recycler</em> de
+            Lars ULRICH pour former un groupe en 1980, c&apos;est James HETFIELD
+            qui a repondu. Le groupe c&apos;est forme a Los Angeles en 1981.
+            Metallica a eu d&apos;autres membres avant ceux que l&apos;on
+            connait actuellement, c&apos;est principalement la place de bassiste
+            qui a le plus change
+          </p>
+          <p>
+            Metallica est l'un des membre du <em>Big Four of Trash</em> au cote
+            de Anthrax, Megadeth et Slayer.
+          </p>
+          <p>
+            En 2012, le groupe fonde son propre label discographique
+            indépendant, Blackened Recordings, et acquiert tous les droits de
+            ses albums en studio. En février 2017 le groupe annonce la fondation
+            de <b>All Within My Hands</b>. La fondation se consacre à la
+            création de communautés durables en soutenant l’éducation de la
+            main-d’œuvre, la lutte contre la faim et d’autres services locaux
+            essentiels. Vous pouvez acceder au site de la fondation via{" "}
+            <a href="https://www.allwithinmyhands.org/">
+              <b>ce lien</b>
+            </a>
+          </p>
         </article>
         <h2>Les membres</h2>
         <article className="homeArticles">
@@ -25,15 +44,7 @@ function Home() {
         </article>
         <h2>Anciens Membres</h2>
         <article className="homeArticles">
-          <ul>
-            <li>
-              Dave MUSTAIN qui a ete exclus, il a ensuite forme le groupe
-              Megadeth
-            </li>
-            <li>Ron McGOVNEY est le premier bassiste de Metallica</li>
-            <li>Cliff BURTON le bassiste mort en 1986 dans un accident</li>
-            <li>Jason NEWSTED qui prend la place de BURTON</li>
-          </ul>
+          <CardAncien />
         </article>
       </section>
     </>
