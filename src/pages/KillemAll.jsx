f5990ae364df9cbb4c584e05cd2c3fcfbde7fe8e
Fabien LAQUERRIERE <fabienlqr@gmail.com>
2025-11-27 20:31:02 +0100

    start kea page

--- src/pages/KillemAll.jsx
+++ src/pages/KillemAll.jsx
@@ -1,8 +1,19 @@
 /* eslint-disable react/no-unescaped-entities */
 function KillemAll() {
-    return ( <>
-    <h1>Kill'em All</h1>
-    <p>Killemall songs</p></> );
+  return (
+    <>
+      <h1>Kill'em All</h1>
+      <section>
+        <article className="homeArticles">
+          <p>
+            L'album Kill'em all est sortie le 25 juillet 1983. C'est le tout
+            premier album qui entrera dans la legende du trash metal. L'album
+            sera enregistre en moins de 3 semaines en mai 1983.
+          </p>
+        </article>
+      </section>
+    </>
+  );
 }
 
 export default KillemAll;
