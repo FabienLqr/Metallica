eb46d4a70146bbabdeb2fddcdb65e6ccfc5b7f18
Fabien LAQUERRIERE <fabienlqr@gmail.com>
2025-11-28 17:48:54 +0100

    First step to add songs on album page's

---
+++ src/components/songs/killemall/seek.jsx
@@ -0,0 +1,125 @@
+function Seek() {
+  return (
+    <>
+      <h3>Seek & Destroy</h3>
+      <p>
+        <time dateTime="PT6M55S">6&apos; 55&quot;</time>
+      </p>
+      <section>
+        <h4>Intro</h4>
+        <p>All right</p>
+      </section>
+      <section>
+        <h4>Verse 1</h4>
+        <p>
+          We&apos;re scanning the scene
+          <br />
+          In the city tonight <br />
+          We&apos;re
+          <br />
+          looking for you <br />
+          To start up a fight <br />
+          There&apos;s an evil feeling <br />
+          In our brains <br />
+          But it&apos;s nothing new <br />
+          You know it drives us insane
+        </p>
+      </section>
+      <section>
+        <h4>Pre-Chorus</h4>
+        <p>
+          Running, on our way, hiding, you will pay <br />
+          Dying, one thousand deaths
+          <br />
+          Running, on our way, hiding, you will pay <br />
+          Dying, one thousand deaths
+        </p>
+      </section>
+      <section>
+        <h4>Chorus</h4>{" "}
+        <p>
+          Searching, seek and destroy <br />
+          Searching, seek and destroy!
+          <br />
+          Searching, seek and destroy <br />
+          Searching, seek and destroy
+        </p>
+      </section>
+      <section>
+        <h4>Verse 2</h4>
+        <p>
+          There is no escape <br />
+          And that&apos;s for sure <br />
+          This is the end <br />
+          We won&apos;t take any more <br />
+          Say goodbye <br />
+          To the world you live in <br />
+          You&apos;ve always been taking <br />
+          And now you&apos;re giving{" "}
+        </p>
+      </section>
+      <section>
+        <h4>Pre-Chorus</h4>
+        <p>
+          Running, on our way, hiding, you will pay <br />
+          Dying, one thousand deaths <br />
+          Running, on our way, hiding, you will pay <br />
+          Dying, one thousand deaths
+        </p>
+      </section>
+      <section>
+        <h4>Chorus</h4>
+        <p>
+          Searching, seek and destroy <br />
+          Searching, seek and destroy! <br />
+          Searching, seek and destroy <br />
+          Searching, seek and destroy!
+        </p>
+      </section>
+      <section>
+        <h4>Instrumental Break</h4>
+      </section>
+      <section>
+        <h4>Guitar Solo</h4>
+      </section>
+      <section>
+        <h4>Verse 3</h4>
+        <p>
+          Our brains are on fire
+          <br /> With the feeling to kill
+          <br /> And it won&apos;t go away
+          <br /> Until our dreams are fulfilled
+          <br />
+          There is only one thing
+          <br /> On our minds <br />
+          Don&apos;t try running away
+          <br />
+          &apos;Cause you&apos;re the one we will find
+        </p>
+      </section>
+      <section>
+        <h4>Pre-Chorus</h4>
+        <p>
+          Running, on our way, hiding, you will pay
+          <br /> Dying, one thousand deaths
+          <br /> Running, on our way, hiding, you will pay
+          <br /> Dying, one thousand deaths{" "}
+        </p>
+      </section>
+      <section>
+        <h4>Chorus</h4>
+        <p>
+          Searching, seek and destroy
+          <br /> Searching, seek and destroy!
+          <br /> Searching, seek and destroy
+          <br /> Searching, seek and destroy, aha-ha-ha-ha
+        </p>
+      </section>
+      <section>
+        <h4>Guitar Solo</h4>
+      </section>
+    </>
+  );
+}
+
+export default Seek;
--- src/pages/KillemAll.jsx
+++ src/pages/KillemAll.jsx
@@ -1,15 +1,20 @@
-/* eslint-disable react/no-unescaped-entities */
+import Seek from "../components/songs/killemall/seek.jsx";
+
 function KillemAll() {
   return (
     <>
-      <h1>Kill'em All</h1>
+      <h1>Kill&apos;em All</h1>
       <section>
         <article className="homeArticles">
           <p>
-            L'album Kill'em all est sortie le 25 juillet 1983. C'est le tout
-            premier album qui entrera dans la legende du trash metal. L'album
-            sera enregistre en moins de 3 semaines en mai 1983.
+            L&apos;album Kill&apos;em all est sortie le 25 juillet 1983.
+            C&apos;est le tout premier album qui entrera dans la legende du
+            trash metal. L&apos;album sera enregistre en moins de 3 semaines en
+            mai 1983.
           </p>
+        </article>
+        <article>
+          <Seek />
         </article>
       </section>
     </>
