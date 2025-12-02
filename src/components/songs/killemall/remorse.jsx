28be12734b5c9daae06cb6561a011d28e81543ec
Fabien LAQUERRIERE <fabienlqr@gmail.com>
2025-12-02 19:12:55 +0100

    Create No Remorse

---
+++ src/components/songs/killemall/remorse.jsx
@@ -0,0 +1,14 @@
+function Remorse() {
+  return (
+    <>
+      <header className="albumHeader">
+        <h3>No Remorse</h3>
+        <p>
+          <time dateTime="PT6M27S">6&apos; 27&quot;</time>
+        </p>
+      </header>
+    </>
+  );
+}
+
+export default Remorse;
