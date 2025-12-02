8d4ae332ba6d22d9ff66558ee6c1320dd70e0a32
Fabien LAQUERRIERE <fabienlqr@gmail.com>
2025-12-02 20:42:46 +0100

    Init and finish Anesthesia

---
+++ src/components/songs/killemall/anesthesia.jsx
@@ -0,0 +1,17 @@
+function Anesthesia() {
+  return (
+    <>
+      <header className="albumHeader">
+        <h3>(Anesthesia) - Pulling Teeth</h3>
+        <p>
+          <time dateTime="PT4M15S">4&apos; 15&quot;</time>
+        </p>
+      </header>
+      <section>
+        <h4>[Pas de lyrics, juste profitez de ce solo de bass incroyable!]</h4>
+      </section>
+    </>
+  );
+}
+
+export default Anesthesia;
