8371bf2e3c516599de0a16a6a4ccedcd55535556
Fabien LAQUERRIERE <fabienlqr@gmail.com>
2025-12-02 19:01:46 +0100

    Create phantom

---
+++ src/components/songs/killemall/phantom.jsx
@@ -0,0 +1,14 @@
+function Phantom() {
+  return (
+    <>
+      <header className="albumHeader">
+        <h3>Motorbreath</h3>
+        <p>
+          <time dateTime="PT3M08S">3&apos; 08&quot;</time>
+        </p>
+      </header>
+    </>
+  );
+}
+
+export default Phantom;
