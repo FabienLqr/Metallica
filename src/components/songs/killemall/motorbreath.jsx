6978297b7985cf2f97a5fa3cfdd3d7adb599c8c3
Fabien LAQUERRIERE <fabienlqr@gmail.com>
2025-12-02 18:57:50 +0100

    Create Motorbreath part

---
+++ src/components/songs/killemall/motorbreath.jsx
@@ -0,0 +1,92 @@
+function Motorbreath() {
+  return (
+    <>
+      <header className="albumHeader">
+        <h3>Motorbreath</h3>
+        <p>
+          <time dateTime="PT3M08S">3&apos; 08&quot;</time>
+        </p>
+      </header>
+      <section>
+        <h4>[Verse 1]</h4>
+        <p>
+          Living and dying, laughing and crying
+          <br />
+          Once you have seen it, you&apos;ll never be the same
+          <br />
+          Life in the fast lane is just how it seems
+          <br />
+          Hard and it&apos;s heavy, it&apos;s dirty and mean
+        </p>
+      </section>
+      <section>
+        <h4>[Chorus]</h4>
+        <p>
+          Motorbreath, it&apos;s how I live my life
+          <br />
+          I can&apos;t take it any other way
+          <br />
+          Motorbreath, the sign of living fast
+          <br />
+          It is going to take your breath away
+        </p>
+      </section>
+      <section>
+        <h4>[Verse 2]</h4>
+        <p>
+          Don&apos;t stop for nothing, it&apos;s full speed or nothing
+          <br />
+          I am taking down you know whatever is in my way
+          <br />
+          Getting your kicks as you are shooting the line
+          <br />
+          Sending the shivers up and down my spine
+        </p>
+      </section>
+      <section>
+        <h4>[Chorus]</h4>
+        <p>
+          Motorbreath, it&apos;s how I live my life
+          <br />
+          I can&apos;t take it any other way
+          <br />
+          Motorbreath, the sign of living fast
+          <br />
+          It is going to take your breath away
+        </p>
+      </section>
+      <section>
+        <h4>[Guitar Solo]</h4>
+      </section>
+      <section>
+        <h4>[Verse 3]</h4>
+        <p>
+          Those people who tell you not to take chances
+          <br />
+          They are all missing on what life&apos;s about
+          <br />
+          You only live once, so take hold of the chance
+          <br />
+          Don&apos;t end up like others, the same song and dance
+        </p>
+      </section>
+      <section>
+        <h4>[Chorus]</h4>
+        <p>
+          Motorbreath, it&apos;s how I live my life
+          <br />
+          I can&apos;t take it any other way
+          <br />
+          Motorbreath, the sign of living fast
+          <br />
+          It is going to take your breath away
+        </p>
+      </section>
+      <section>
+        <h4>[Guitar Solo]</h4>
+      </section>
+    </>
+  );
+}
+
+export default Motorbreath;
