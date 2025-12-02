0372650e64855bd4b7cd2effcf2323e0100510b9
Fabien LAQUERRIERE <fabienlqr@gmail.com>
2025-12-02 19:58:57 +0100

    Finish Hit the Lights

--- src/components/songs/killemall/light.jsx
+++ src/components/songs/killemall/light.jsx
@@ -8,8 +8,102 @@ function Light() {
         </p>
       </header>
       <section>
-        <h4></h4>
-        <p></p>
+        <h4>[Verse 1]</h4>
+        <p>
+          No life ‘til leather
+          <br />
+          We&apos;re gonna kick some ass tonight
+          <br />
+          When our fans start screaming, it&apos;s right
+          <br />
+          Well, all right, yeah
+        </p>
+      </section>
+      <section>
+        <h4>[Pre-Chorus]</h4>
+        <p>
+          When we start to rock
+          <br />
+          We never wanna stop again
+        </p>
+      </section>
+      <section>
+        <h4>[Chorus]</h4>
+        <p>
+          Hit the lights!
+          <br />
+          Hit the lights!
+          <br />
+          Hit the lights!
+        </p>
+      </section>
+      <section>
+        <h4>[Verse 2]</h4>
+        <p>
+          You know our fans are insane
+          <br />
+          With volume higher
+          <br />
+          Than anything today
+          <br />
+          The only way, yeah
+        </p>
+      </section>
+      <section>
+        <h4>[Pre-Chorus]</h4>
+        <p>
+          When we start to rock
+          <br />
+          We never wanna stop again
+        </p>
+      </section>
+      <section>
+        <h4>[Chorus]</h4>
+        <p>
+          Hit the lights!
+          <br />
+          Hit the lights!
+          <br />
+          Hit the lights!
+        </p>
+      </section>
+      <section>
+        <h4>[Verse 3]</h4>
+        <p>
+          With all-out screaming
+          <br />
+          We&apos;re gonna rip right through your brain
+          <br />
+          We got the lethal power
+          <br />
+          It is causing you sweet pain
+          <br />
+          Oh, sweet pain, yeah
+        </p>
+      </section>
+      <section>
+        <h4>[Pre-Chorus]</h4>
+        <p>
+          When we start to rock
+          <br />
+          We never wanna stop again
+        </p>
+      </section>
+      <section>
+        <h4>[Chorus]</h4>
+        <p>
+          Hit the lights!
+          <br />
+          Hit the lights!
+          <br />
+          Hit the lights!
+        </p>
+      </section>
+      <section>
+        <h4>[Instrumental Break]</h4>
+      </section>
+      <section>
+        <h4>[Guitar Solo]</h4>
       </section>
     </>
   );
