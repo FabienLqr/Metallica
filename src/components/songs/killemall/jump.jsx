bcd4cd9737e9799f75a966e23a45bee8948d5557
Fabien LAQUERRIERE <fabienlqr@gmail.com>
2025-12-02 20:11:24 +0100

    Init Jump in the Fire

---
+++ src/components/songs/killemall/jump.jsx
@@ -0,0 +1,18 @@
+function Jump() {
+  return (
+    <>
+      <header className="albumHeader">
+        <h3>Jump in the Fire</h3>
+        <p>
+          <time dateTime="PT4M41S">4&apos; 41&quot;</time>
+        </p>
+      </header>
+      <section>
+        <h4></h4>
+        <p></p>
+      </section>
+    </>
+  );
+}
+
+export default Jump;
