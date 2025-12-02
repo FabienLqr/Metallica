f1c22890400943af002a5fedecc9a28b2a1c2ec3
Fabien LAQUERRIERE <fabienlqr@gmail.com>
2025-12-02 19:20:13 +0100

    Finih No remorse

--- src/components/songs/killemall/remorse.jsx
+++ src/components/songs/killemall/remorse.jsx
@@ -7,6 +7,147 @@ function Remorse() {
           <time dateTime="PT6M27S">6&apos; 27&quot;</time>
         </p>
       </header>
+      <section>
+        <h4>[Guitar Solo]</h4>
+      </section>
+      <section>
+        <h4>[Verse 1]</h4>
+        <p>
+          No mercy for what we&apos;re doing
+          <br />
+          No thought to even what we&apos;ve done
+          <br />
+          We don&apos;t need to feel the sorrow
+          <br />
+          No remorse for the helpless one
+        </p>
+      </section>
+      <section>
+        <h4>[Pre-Chorus]</h4>
+        <p>War without end</p>
+      </section>
+      <section>
+        <h4>[Chorus]</h4>
+        <p>
+          No remorse, no repent
+          <br />
+          We don&apos;t care what it meant
+          <br />
+          Another day, another death
+          <br />
+          Another sorrow, another breath
+          <br />
+          No remorse, no repent
+          <br />
+          We don&apos;t care what it meant
+          <br />
+          Another day, another death
+          <br />
+          Another sorrow, another breath
+        </p>
+      </section>
+      <section>
+        <h4>[Verse 2]</h4>
+        <p>
+          Blood feeds the war machine
+          <br />
+          As it eats its way across the land
+          <br />
+          We don&apos;t need to feel the sorrow
+          <br />
+          &quot;No remorse&quot; is the one command
+        </p>
+      </section>
+      <section>
+        <h4>[Pre-Chorus]</h4>
+        <p>War without end</p>
+      </section>
+      <section>
+        <h4>[Chorus]</h4>
+        <p>
+          No remorse, no repent
+          <br />
+          We don&apos;t care what it meant
+          <br />
+          Another day, another death
+          <br />
+          Another sorrow, another breath
+          <br />
+          No remorse, no repent
+          <br />
+          We don&apos;t care what it meant
+          <br />
+          Another day, another death
+          <br />
+          Another sorrow, another breath
+        </p>
+      </section>
+      <section>
+        <h4>[Instrumental Break]</h4>
+      </section>
+      <section>
+        <h4>[Guitar Solo]</h4>
+      </section>
+      <section>
+        <h4>[Verse 3]</h4>
+        <p>
+          Only the strong survive
+          <br />
+          No will to save the weaker race
+          <br />
+          We&apos;re ready to kill all comers
+          <br />
+          Like a loaded gun right at your face
+        </p>
+      </section>
+      <section>
+        <h4>[Pre-Chorus]</h4>
+        <p>War without end</p>
+      </section>
+      <section>
+        <h4>[Chorus]</h4>
+        <p>
+          No remorse, no repent
+          <br />
+          We don&apos;t care what it meant
+          <br />
+          Another day, another death
+          <br />
+          Another sorrow, another breath
+          <br />
+          No remorse, no repent
+          <br />
+          We don&apos;t care what it meant
+          <br />
+          Another day, another death
+          <br />
+          Another sorrow, another breath
+        </p>
+      </section>
+      <section>
+        <h4>[Bridge]</h4>
+        <p>
+          Attack! Bullets are flying, people are dying
+          <br />
+          With madness surrounding, all hell&apos;s breaking loose
+          <br />
+          Soldiers are hounding, bodies are mounting
+          <br />
+          Cannons are shouting to take their abuse
+          <br />
+          With war machines going, blood starts to flowing
+          <br />
+          No mercy given to anyone here
+          <br />
+          The furious fighting, swords are like lightning
+          <br />
+          It all becomes frightening, you know death is near
+        </p>
+      </section>
+      <section>
+        <h4>[Outro]</h4>
+        <p>No remorse</p>
+      </section>
     </>
   );
 }
