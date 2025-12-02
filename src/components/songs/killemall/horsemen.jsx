e081c7d1c521bfd4e5a00e9bf2c8f2a7b460e60d
Fabien LAQUERRIERE <fabienlqr@gmail.com>
2025-12-02 19:24:35 +0100

    Create Horsemen

---
+++ src/components/songs/killemall/horsemen.jsx
@@ -0,0 +1,18 @@
+function Horsemen() {
+  return (
+    <>
+      <header className="albumHeader">
+        <h3>Metal Militia</h3>
+        <p>
+          <time dateTime="PT5M11S">5&apos; 11&quot;</time>
+        </p>
+      </header>
+      <section>
+        <h4></h4>
+        <p></p>
+      </section>
+    </>
+  );
+}
+
+export default Horsemen;
