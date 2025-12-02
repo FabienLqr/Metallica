889733ee1830bfa409a8d99871c8d9413b8409d6
Fabien LAQUERRIERE <fabienlqr@gmail.com>
2025-12-02 19:51:57 +0100

    Create Hit the Lights

---
+++ src/components/songs/killemall/light.jsx
@@ -0,0 +1,18 @@
+function Light() {
+  return (
+    <>
+      <header className="albumHeader">
+        <h3>Hit the Lights</h3>
+        <p>
+          <time dateTime="PT4M17S">4&apos; 17&quot;</time>
+        </p>
+      </header>
+      <section>
+        <h4></h4>
+        <p></p>
+      </section>
+    </>
+  );
+}
+
+export default Light;
