3f9ae79746d4d5a4a8451a6c640dd5e5bcee8f49
Fabien LAQUERRIERE <fabienlqr@gmail.com>
2025-12-02 18:26:42 +0100

    Create Metal Militia page

---
+++ src/components/songs/killemall/militia.jsx
@@ -0,0 +1,151 @@
+function Militia() {
+  return (
+    <>
+      <header className="albumHeader">
+        <h3>Metal Militia</h3>
+        <p>
+          <time dateTime="PT5M11S">5&apos; 11&quot;</time>
+        </p>
+      </header>
+      <section>
+        <h4>[Verse 1]</h4>
+        <p>
+          Thunder and lightning, the gods take revenge
+          <br />
+          Senseless destruction
+          <br />
+          Victims of fury are cowardly now
+          <br />
+          Running for safety
+          <br />
+          Stabbing the harlot to pay for her sins
+          <br />
+          Leaving the virgin
+          <br />
+          Suicide running as if it were free
+          <br />
+          Ripping and tearing
+        </p>
+      </section>
+      <section>
+        <h4>[Pre-Chorus]</h4>
+        <p>
+          Oh, through the mist and the madness
+          <br />
+          We&apos;re trying to get the message to you
+        </p>
+      </section>
+      <section>
+        <h4>[Chorus]</h4>
+        <p>
+          Metal militia
+          <br />
+          Metal militia
+          <br />
+          Metal militia
+        </p>
+      </section>
+      <section>
+        <h4>[Verse 2]</h4>
+        <p>
+          Chained and shadowed to be left behind
+          <br />
+          Nine and one thousand
+          <br />
+          Metal militia for your sacrifice
+          <br />
+          Ironclad soldiers
+          <br />
+          Join or be conquered the law of the land
+          <br />
+          What will befall you
+          <br />
+          The metallization of your inner soul
+          <br />
+          Twisting and turning
+        </p>
+      </section>
+      <section>
+        <h4>[Pre-Chorus]</h4>
+        <p>
+          Oh, through the mist and the madness
+          <br />
+          We&apos;re trying to get the message to you
+        </p>
+      </section>
+      <section>
+        <h4>[Chorus]</h4>
+        <p>
+          Metal militia
+          <br />
+          Metal militia, oh!
+        </p>
+      </section>
+      <section>
+        <h4>[Guitar Solo]</h4>
+      </section>
+      <section>
+        <h4>[Instrumental Break]</h4>
+        <p>Oh!</p>
+      </section>
+      <section>
+        <h4>[Verse 3]</h4>
+        <p>
+          We are as one; yes, we all are the same
+          <br />
+          Fighting for one cause
+          <br />
+          Leather and metal are our uniforms
+          <br />
+          Protecting what we are
+          <br />
+          Joining together to take on the world
+          <br />
+          With our heavy metal
+          <br />
+          Spreading the message to everyone here
+          <br />
+          Come let yourself go
+        </p>
+      </section>
+      <section>
+        <h4>[Pre-Chorus]</h4>
+        <p>
+          Oh, through the mist and the madness
+          <br />
+          We&apos;re trying to get the message to you
+        </p>
+      </section>
+      <section>
+        <h4>[Chorus]</h4>
+        <p>
+          Metal militia
+          <br />
+          Metal militia
+        </p>
+      </section>
+      <section>
+        <h4>[Outro]</h4>
+        <p>
+          Metal militia
+          <br />
+          Metal militia
+          <br />
+          Metal militia
+          <br />
+          Metal militia
+          <br />
+          Metal militia
+          <br />
+          Metal militia
+          <br />
+          Metal militia
+          <br />
+          Metal militia
+        </p>
+      </section>
+    </>
+  );
+}
+
+export default Militia;
