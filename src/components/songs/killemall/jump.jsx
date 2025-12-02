aa0692a73d50003f239e718940325e0d66283ecd
Fabien LAQUERRIERE <fabienlqr@gmail.com>
2025-12-02 20:22:04 +0100

    Finish Jump on the Fire

--- src/components/songs/killemall/jump.jsx
+++ src/components/songs/killemall/jump.jsx
@@ -8,8 +8,119 @@ function Jump() {
         </p>
       </header>
       <section>
-        <h4></h4>
-        <p></p>
+        <h4>[Verse 1]</h4>
+        <p>
+          Down in the depths of my fiery home
+          <br />
+          The summons bell will chime
+          <br />
+          Tempting you and all the Earth
+          <br />
+          To join our sinful kind
+          <br />
+          There is a job to be done and I&apos;m the one
+          <br />
+          You people make me do it
+          <br />
+          Now it&apos;s time for your fate and I won&apos;t hesitate
+          <br />
+          To pull you down into this pit
+        </p>
+      </section>
+      <section>
+        <h4>[Chorus]</h4>
+        <p>
+          So come on
+          <br />
+          Jump in the fire
+          <br />
+          So come on
+          <br />
+          Jump in the fire
+        </p>
+      </section>
+      <section>
+        <h4>[Verse 2]</h4>
+        <p>
+          With hell in my eyes and with death in my veins
+          <br />
+          The end is closing in
+          <br />
+          Feeding on the minds of man
+          <br />
+          And from their souls within
+          <br />
+          My disciples all shout to search you out
+          <br />
+          And they always shall obey
+          <br />
+          Follow me now, my child, not the meek or the mild
+          <br />
+          But do just as I say
+        </p>
+      </section>
+      <section>
+        <h4>[Chorus]</h4>
+        <p>
+          So come on
+          <br />
+          Jump in the fire
+          <br />
+          So come on
+          <br />
+          Jump in the fire
+        </p>
+      </section>
+      <section>
+        <h4>[Instrumental Break]</h4>
+      </section>
+      <section>
+        <h4>[Guitar Solo]</h4>
+      </section>
+      <section>
+        <h4>[Verse 3]</h4>
+        <p>
+          Jump by your will or be taken by force
+          <br />
+          I&apos;ll get you either way
+          <br />
+          Trying to keep the hellfire lit
+          <br />
+          I&apos;m stalking you as prey
+          <br />
+          Living your life with me, I am you, you see
+          <br />
+          There is part of me in everyone
+          <br />
+          So reach down grab my hand, walk with me through the land
+          <br />
+          Come home where you belong
+        </p>
+      </section>
+      <section>
+        <h4>[Chorus]</h4>
+        <p>
+          So come on
+          <br />
+          Jump in the fire
+          <br />
+          So come on
+          <br />
+          Jump in the fire
+          <br />
+          So come on
+          <br />
+          Jump in the fire
+          <br />
+          So come on
+          <br />
+          Jump in the fire
+          <br />
+          Come on, jump in now!
+        </p>
+      </section>
+      <section>
+        <h4>[Guitar Solo]</h4>
       </section>
     </>
   );
