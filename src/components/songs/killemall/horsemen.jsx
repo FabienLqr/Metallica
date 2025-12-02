f093b34bca008f508d960be31a73575bf0c80202
Fabien LAQUERRIERE <fabienlqr@gmail.com>
2025-12-02 19:30:34 +0100

    Finish The Four Horsemen

--- src/components/songs/killemall/horsemen.jsx
+++ src/components/songs/killemall/horsemen.jsx
@@ -2,14 +2,141 @@ function Horsemen() {
   return (
     <>
       <header className="albumHeader">
-        <h3>Metal Militia</h3>
+        <h3>The Four Horsemen</h3>
         <p>
-          <time dateTime="PT5M11S">5&apos; 11&quot;</time>
+          <time dateTime="PT7M13S">7&apos; 13&quot;</time>
         </p>
       </header>
       <section>
-        <h4></h4>
-        <p></p>
+        <h4>[Verse 1]</h4>
+        <p>
+          By the last breath of the fourth winds blow
+          <br />
+          Better raise your ears
+          <br />
+          The sound of hooves knock at your door
+          <br />
+          Lock up your wife and children now
+          <br />
+          It&apos;s time to wield the blade
+          <br />
+          For now you&apos;ve got some company
+        </p>
+      </section>
+      <section>
+        <h4>[Chorus]</h4>
+        <p>
+          he horsemen are drawing nearer
+          <br />
+          On the leather steeds they ride
+          <br />
+          They&apos;ve come to take your life
+          <br />
+          On through the dead of night
+          <br />
+          With the four horsemen ride
+          <br />
+          Or choose your fate and die
+          <br />
+          Aw yeah, yeah!
+        </p>
+      </section>
+      <section>
+        <h4>[Verse 2]</h4>
+        <p>
+          You&apos;ve been dying since the day you were born
+          <br />
+          You know it&apos;s all been planned
+          <br />
+          The quartet of deliverance rides
+          <br />
+          A sinner once a sinner twice
+          <br />
+          No need for confession now
+          <br />
+          &apos;Cause now you have got the fight of your life
+        </p>
+      </section>
+      <section>
+        <h4>[Chorus]</h4>
+        <p>
+          The horsemen are drawing nearer
+          <br />
+          On the leather steeds they ride
+          <br />
+          They&apos;ve come to take your life
+          <br />
+          On through the dead of night
+          <br />
+          With the four horsemen ride
+          <br />
+          Or choose your fate and die
+          <br />
+          Aw yeah, yeah!
+        </p>
+      </section>
+      <section>
+        <h4>[Instrumental Break]</h4>
+      </section>
+      <section>
+        <h4>[Bridge]</h4>
+        <p>
+          Time! Has taken its toll on you
+          <br />
+          The lines that crack your face
+          <br />
+          Famine! Your body, it has torn through
+          <br />
+          Withered in every place
+          <br />
+          Pestilence! For what you&apos;ve had to endure
+          <br />
+          And what you have put others through
+          <br />
+          Death! Deliverance for you for sure
+          <br />
+          Now there&apos;s nothing you can do
+        </p>
+      </section>
+      <section>
+        <h4>[Instrumental Break]</h4>
+      </section>
+      <section>
+        <h4>[Guitar Solo]</h4>
+      </section>
+      <section>
+        <h4>[Verse 3]</h4>
+        <p>
+          So gather round, young warriors, now
+          <br />
+          And saddle up your steeds
+          <br />
+          Killing scores with demon swords
+          <br />
+          Now&apos;s the death of doers of wrong
+          <br />
+          Swing the judgment hammer down
+          <br />
+          Safely inside armor: blood, guts, and sweat
+        </p>
+      </section>
+      <section>
+        <h4>[Chorus]</h4>
+        <p>
+          The horsemen are drawing nearer
+          <br />
+          On the leather steeds they ride
+          <br />
+          They&apos;ve come to take your life
+          <br />
+          On through the dead of night
+          <br />
+          With the four horsemen ride
+          <br />
+          Or choose your fate and die
+          <br />
+          Aw yeah, yeah!
+        </p>
       </section>
     </>
   );
