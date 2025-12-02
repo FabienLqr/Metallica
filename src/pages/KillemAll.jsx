0f4855846fd2bf058f0e30d43c97c8594c4ce2c8
Fabien LAQUERRIERE <fabienlqr@gmail.com>
2025-12-02 18:29:43 +0100

    Add mention in KEA album

--- src/pages/KillemAll.jsx
+++ src/pages/KillemAll.jsx
@@ -1,3 +1,4 @@
+//import des musiques
 import Seek from "../components/songs/killemall/seek.jsx";
 
 // import des images
@@ -7,18 +8,14 @@ function KillemAll() {
   return (
     <>
       <h1>Kill&apos;em All</h1>
-      <img
-                  src={kill}
-                  alt="Logo de l'album Kill'em All"
-                  className="album"
-                />
+      <img src={kill} alt="Logo de l'album Kill'em All" className="album" />
       <section>
         <article className="homeArticles">
           <p>
             L&apos;album Kill&apos;em all est sortie le 25 juillet 1983.
-            C&apos;est le tout premier album du groupe qui entrera dans la legende du
-            trash metal. L&apos;album sera enregistre en moins de 3 semaines en
-            mai 1983.
+            C&apos;est le tout premier album du groupe qui entrera dans la
+            legende du trash metal. L&apos;album sera enregistre en moins de 3
+            semaines en mai 1983.
           </p>
         </article>
         <article className="song">
