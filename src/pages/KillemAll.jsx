f34ea325e165c162c1af286e30d5811e3f50ebbc
Fabien LAQUERRIERE <fabienlqr@gmail.com>
2025-12-02 18:59:55 +0100

    Import Motorbreath in KEA album

--- src/pages/KillemAll.jsx
+++ src/pages/KillemAll.jsx
@@ -1,6 +1,7 @@
 //import des musiques
 import Seek from "../components/songs/killemall/seek.jsx";
 import Militia from "../components/songs/killemall/militia.jsx";
+import Motorbreath from "../components/songs/killemall/motorbreath.jsx";
 
 // import des images
 import kill from "../assets/Images/KILL'EMALL.jpg";
@@ -20,8 +21,10 @@ function KillemAll() {
           </p>
         </article>
         <article className="song">
+          {/*il faut une gestion des musiques pour n'en afficher qu'une a la fois, menu deroulant? avec peut etre seek en valeur par defaut?*/}
           <Seek />
           <Militia />
+          <Motorbreath />
         </article>
       </section>
     </>
