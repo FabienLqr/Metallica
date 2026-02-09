bc74a90ff9eff6fef9e61320816c53a7806e39ed
Fabien LAQUERRIERE <fabienlqr@gmail.com>
2026-02-09 09:10:47 +0100

    ADD hardwired

--- src/pages/Hardwired.jsx
+++ src/pages/Hardwired.jsx
@@ -1,7 +1,15 @@
+//import image
+import hardwiredImg from "../assets/Images/Hardwired.jpg"
+
 function Hardwired() {
   return (
     <>
       <h1>Hardwired to Self_Destruct</h1>
+      <img
+              src={hardwiredImg}
+              alt="Logo de l'album Ride The Lightning"
+              className="album"
+            />
       <p>Hardwired to Self-Destruct song</p>
     </>
   );
