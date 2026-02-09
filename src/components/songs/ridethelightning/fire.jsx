f47f9aa4dbdf28eb04063514180bd9f6fc0a0eaa
Fabien LAQUERRIERE <fabienlqr@gmail.com>
2026-02-09 11:30:10 +0100

    ADD lyrics of Fight Fire With Fire

--- src/components/songs/ridethelightning/fire.jsx
+++ src/components/songs/ridethelightning/fire.jsx
@@ -12,7 +12,91 @@ function Fire() {
         <Player music={fireMusic} />
       </header>
       <section>
-        <h4>[Comes later]</h4>
+        <h4>[Instrumental Intro]</h4>
+      </section>
+      <section>
+        <h4>[Verse 1]</h4>
+        <p>
+          Do unto others as they&apos;ve done to you
+          <br />
+          But what the hell is this world coming to?
+          <br />
+          Blow the universe into nothingness <br />
+          Nuclear warfare shall lay us to rest
+        </p>
+      </section>
+      <section>
+        <h4>[Chorus]</h4>
+        <p>
+          Fight fire with fire
+          <br />
+          Ending is near
+          <br />
+          Fight fire with fire
+          <br />
+          Bursting with fear
+          <br />
+          We all shall die
+        </p>
+      </section>
+      <section>
+        <h4>[Verse 2]</h4>
+        <p>
+          Time is like a fuse, short and burning fast
+          <br />
+          Armageddon&apos;s here, like said in the past
+        </p>
+      </section>
+      <section>
+        <h4>[Chorus]</h4>
+        <p>
+          Fight fire with fire
+          <br />
+          Ending is near
+          <br />
+          Fight fire with fire
+          <br />
+          Bursting with fear
+          <br />
+        </p>
+      </section>
+      <section>
+        <h4>[Guitar Solo]</h4>
+      </section>
+      <section>
+        <h4>[Instrumental Break]</h4>
+      </section>
+      <section>
+        <h4>[Verse 3]</h4>
+        <p>
+          Soon to fill our lungs, the hot winds of death
+          <br />
+          The gods are laughing, so take your last breath
+        </p>
+      </section>
+      <section>
+        <h4>[Chorus]</h4>
+        <p>
+          Fight fire with fire
+          <br />
+          Ending is near
+          <br />
+          Fight fire with fire
+          <br />
+          Bursting with fear
+        </p>
+      </section>
+      <section>
+        <h4>[Outro]</h4>
+        <p>
+          Fight fire with fire, fight fire with fire
+          <br />
+          Fight fire with fire, fight fire with fire
+          <br />
+          Fight fire with fire, fight fire with fire
+          <br />
+          Fight fire with fire, fight fire with fire, fight
+        </p>
       </section>
     </>
   );
