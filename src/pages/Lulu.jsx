a166ac7b134090f248cc392433c60a1e92ab86f4
Fabien LAQUERRIERE <fabienlqr@gmail.com>
2026-02-09 09:22:00 +0100

    ADD lulu

--- src/pages/Lulu.jsx
+++ src/pages/Lulu.jsx
@@ -1,7 +1,15 @@
+//import image
+import luluImg from "../assets/Images/Lulu.jpg"
+
 function Lulu() {
   return (
     <>
       <h1>Lulu</h1>
+      <img
+              src={luluImg}
+              alt="Logo de l'album Ride The Lightning"
+              className="album"
+            />
       <p>Lulu song</p>
     </>
   );
