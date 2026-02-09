5b3457e915ebc58387096b6d5ee0703fecbca0d7
Fabien LAQUERRIERE <fabienlqr@gmail.com>
2026-02-09 09:15:03 +0100

    ADD sm

--- src/pages/Sm.jsx
+++ src/pages/Sm.jsx
@@ -1,7 +1,15 @@
+//import image
+import smImg from "../assets/Images/SM.jpg"
+
 function Sm() {
   return (
     <>
       <h1>S&M</h1>
+      <img
+              src={smImg}
+              alt="Logo de l'album Ride The Lightning"
+              className="album"
+            />
       <p>S&M song</p>
     </>
   );
