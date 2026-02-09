ec111eea48f022009d6dd8c5721e5e5aa6c0807c
Fabien LAQUERRIERE <fabienlqr@gmail.com>
2026-02-09 08:49:13 +0100

    ADD master

--- src/pages/MasterofPuppets.jsx
+++ src/pages/MasterofPuppets.jsx
@@ -1,6 +1,14 @@
+//import des images
+import masterImg from "../assets/Images/Master.jpg"
+
 function MasterofPuppets() {
     return ( <>
         <h1>Master of puppets</h1>
+        <img
+                src={masterImg}
+                alt="Logo de l'album Ride The Lightning"
+                className="album"
+              />
         <p>Master of puppets songs</p></>  );
 }
 
