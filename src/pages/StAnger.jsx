f9e87fe969cdeedbd8fde8a0550ad56ccf56c15f
Fabien LAQUERRIERE <fabienlqr@gmail.com>
2026-02-09 09:05:51 +0100

    ADD anger

--- src/pages/StAnger.jsx
+++ src/pages/StAnger.jsx
@@ -1,5 +1,13 @@
+//import image
+import angerImg from "../assets/Images/Anger.jpg"
+
 function StAnger() {
     return ( <><h1>St. Anger</h1>
+    \<img
+            src={angerImg}
+            alt="Logo de l'album Ride The Lightning"
+            className="album"
+          />
     <p>St. Anger song</p></> );
 }
 
