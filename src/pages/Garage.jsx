8f6e9685f3082e3c09fc696481f03706722f3ccc
Fabien LAQUERRIERE <fabienlqr@gmail.com>
2026-02-09 09:19:04 +0100

    ADD garage

--- src/pages/Garage.jsx
+++ src/pages/Garage.jsx
@@ -1,7 +1,15 @@
+//import image
+import garageImg from "../assets/Images/Garage.jpg"
+
 function Garage() {
   return (
     <>
       <h1>Garage Inc.</h1>
+      <img
+              src={garageImg}
+              alt="Logo de l'album Ride The Lightning"
+              className="album"
+            />
       <p>Garage Inc. song</p>
     </>
   );
