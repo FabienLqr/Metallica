b96ebcbc4bd651026ad4c08e354267cd9c4e4249
Fabien LAQUERRIERE <fabienlqr@gmail.com>
2026-02-09 09:12:46 +0100

    ADD seasons

--- src/pages/Seasons.jsx
+++ src/pages/Seasons.jsx
@@ -1,7 +1,15 @@
+//import image
+import seasonsImg from "../assets/Images/Seasons.jpg"
+
 function Seasons() {
   return (
     <>
       <h1>72 Seasons</h1>
+      <img
+              src={seasonsImg}
+              alt="Logo de l'album Ride The Lightning"
+              className="album"
+            />
       <p>72 Seasons song</p>
     </>
   );
