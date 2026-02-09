d55d98bb820ddc1a654260491b46b87d91d5a8df
Fabien LAQUERRIERE <fabienlqr@gmail.com>
2026-02-09 08:54:50 +0100

    ADD black

--- src/pages/BlackAlbum.jsx
+++ src/pages/BlackAlbum.jsx
@@ -1,6 +1,14 @@
+//import image
+import blackImg from "../assets/Images/Black.jpg"
+
 function BlackAlbum() {
     return ( <>
         <h1>Black Album</h1>
+        <img
+                src={blackImg}
+                alt="Logo de l'album Ride The Lightning"
+                className="album"
+              />
         <p>Black Album songs</p></>  );
 }
 
