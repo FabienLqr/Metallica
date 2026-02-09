a1862252cf9bb27b7d7fc2e2e03d148088d1e6ee
Fabien LAQUERRIERE <fabienlqr@gmail.com>
2026-02-09 08:52:54 +0100

    ADD justice

--- src/pages/Justice.jsx
+++ src/pages/Justice.jsx
@@ -1,6 +1,14 @@
+//import image
+import justiceImg from "../assets/Images/Justice.jpg"
+
 function Justice() {
     return ( <>
         <h1>And Justice for All</h1>
+        <img
+                src={justiceImg}
+                alt="Logo de l'album Ride The Lightning"
+                className="album"
+              />
         <p>And Justice for all songs</p></>  );
 }
 
