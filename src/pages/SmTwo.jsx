599080aff3a7f72554d07792b881bec8195fe863
Fabien LAQUERRIERE <fabienlqr@gmail.com>
2026-02-09 09:17:39 +0100

    ADD sm2

--- src/pages/SmTwo.jsx
+++ src/pages/SmTwo.jsx
@@ -1,7 +1,15 @@
+//import image
+import sm2Img from "../assets/Images/SM2.jpg"
+
 function SmTwo() {
   return (
     <>
       <h1>S&M2</h1>
+      <img
+              src={sm2Img}
+              alt="Logo de l'album Ride The Lightning"
+              className="album"
+            />
       <p>S&M2 song</p>
     </>
   );
