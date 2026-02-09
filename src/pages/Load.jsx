f6a418cab5e3b248e2eb4dd06b9f7e48fc410ca7
Fabien LAQUERRIERE <fabienlqr@gmail.com>
2026-02-09 08:56:14 +0100

    ADD load

--- src/pages/Load.jsx
+++ src/pages/Load.jsx
@@ -1,6 +1,14 @@
+//import image
+import loadImg from "../assets/Images/Load.jpg"
+
 function Load() {
     return ( <>
         <h1>Load</h1>
+        <img
+                src={loadImg}
+                alt="Logo de l'album Ride The Lightning"
+                className="album"
+              />
         <p>Load songs</p></>  );
 }
 
