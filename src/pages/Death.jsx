745cc988b4788d8c3169eb5d1a889a510fc2a22c
Fabien LAQUERRIERE <fabienlqr@gmail.com>
2026-02-09 09:07:34 +0100

    ADD death

--- src/pages/Death.jsx
+++ src/pages/Death.jsx
@@ -1,7 +1,15 @@
+//import image
+import deathImg from "../assets/Images/Magnetic.jpg"
+
 function Death() {
   return (
     <>
       <h1>Death Magnetic</h1>
+      <img
+              src={deathImg}
+              alt="Logo de l'album Ride The Lightning"
+              className="album"
+            />
       <p>Death Magnetic song</p>
     </>
   );
