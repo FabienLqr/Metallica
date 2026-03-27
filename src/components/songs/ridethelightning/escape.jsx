eef78d6f22414c405fcc4b2dbc29e830e3397819
Fabien LAQUERRIERE <fabienlqr@gmail.com>
2026-03-27 14:42:28 +0100

    SET lyrics of Escape

--- src/components/songs/ridethelightning/escape.jsx
+++ src/components/songs/ridethelightning/escape.jsx
@@ -1,19 +1,118 @@
 import Player from "../../Player";
 import escapeMusic from "../../../assets/musics/rideTheLightning/escape.mp3";
 
-function Escape () {
-    return(<>
-          <header className="albumHeader">
-            <h3>Escape</h3>
-            <p>
-              <time dateTime="PT4M23S">4&apos; 23&quot;</time>
-            </p>
-            <Player music={escapeMusic} />
-          </header>
-          <section>
-            <h4>[Comes later]</h4>
-          </section>
-        </>)
+function Escape() {
+  return (
+    <>
+      <header className="albumHeader">
+        <h3>Escape</h3>
+        <p>
+          <time dateTime="PT4M23S">4&apos; 23&quot;</time>
+        </p>
+        <Player music={escapeMusic} />
+      </header>
+      <section>
+        <h4>[Instrumental Intro]</h4>
+      </section>
+      <section>
+        <h4>[Verse 1]</h4>
+        <p>
+          Feel no pain, but my life ain&apos;t easy
+          <br />
+          I know I&apos;m my best friend
+          <br />
+          No one cares, but I&apos;m so much stronger
+          <br />
+          I&apos;ll fight until the end
+          <br />
+          To escape from the true false world
+          <br />
+          Undamaged destiny
+          <br />
+          Can&apos;t get caught in the endless circle
+          <br />
+          Ring of stupidity
+        </p>
+      </section>
+      <section>
+        <h4>[Chorus]</h4>
+        <p>
+          Out for my own, out to be free
+          <br />
+          One with my mind, they just can&apos;t see
+          <br />
+          No need to hear things that they say
+          <br />
+          Life&apos;s for my own to live my own way
+        </p>
+      </section>
+      <section>
+        <h4>[Verse 2]</h4>
+        <p>
+          Rape my mind and destroy my feelings
+          <br />
+          Don&apos;t tell me what to do
+          <br />
+          I don&apos;t care now, &apos;cause I&apos;m on my side
+          <br />
+          And I can see through you
+          <br />
+          Feed my brain with your so-called standards
+          <br />
+          Who says that I ain&apos;t right?
+          <br />
+          Break away from your common fashion
+          <br />
+          See through your blurry sight
+        </p>
+      </section>
+      <section>
+        <h4>[Chorus]</h4>
+        <p>
+          Out for my own, out to be free
+          <br />
+          One with my mind, they just can&apos;t see
+          <br />
+          No need to hear things that they say
+          <br />
+          Life&apos;s for my own to live my own way
+        </p>
+      </section>
+      <section>
+        <h4>[Bridge]</h4>
+        <p>
+          See them try to bring the hammer down
+          <br />
+          No damn chains can hold me to the ground
+        </p>
+      </section>
+      <section>
+        <h4>[Guitar Solo]</h4>
+      </section>
+      <section>
+        <h4>[Outro]</h4>
+        <p>
+          Life&apos;s for my own to live my own way
+          <br />
+          Life&apos;s for my own to live my own way
+          <br />
+          Life&apos;s for my own to live my own way
+          <br />
+          Life&apos;s for my own to live my own way
+          <br />
+          Life&apos;s for my own to live my own way
+          <br />
+          Life&apos;s for my own to live my own way
+          <br />
+          Life&apos;s for my own to live my own way
+          <br />
+          Life&apos;s for my own to live my own way
+          <br />
+          Life&apos;s for my own to live my own way
+        </p>
+      </section>
+    </>
+  );
 }
 
 export default Escape;
