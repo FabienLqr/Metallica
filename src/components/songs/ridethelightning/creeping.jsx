17aae787dafd5f82dfecb694e129d89d3dfb6221
Fabien LAQUERRIERE <fabienlqr@gmail.com>
2026-03-27 14:56:22 +0100

    SET lyrics of Creeping death

--- src/components/songs/ridethelightning/creeping.jsx
+++ src/components/songs/ridethelightning/creeping.jsx
@@ -12,7 +12,110 @@ function Creeping() {
         <Player music={creepingMusic} />
       </header>
       <section>
-        <h4>[Comes later]</h4>
+        <h4>[Instrumental Intro]</h4>
+      </section>
+      <section>
+        <h4>[Verse 1]</h4>
+        <p>
+          Slaves, Hebrews born to serve to the Pharaoh
+          <br />
+          Heed to his every word, live in fear
+          <br />
+          Faith of the unknown one, the deliverer
+          <br />
+          Wait, something must be done, four hundred years
+        </p>
+      </section>
+      <section>
+        <h4>[Chorus]</h4>
+        <p>
+          So let it be written, so let it be done
+          <br />
+          I&apos;m sent here by the chosen one
+          <br />
+          So let it be written, so let it be done
+          <br />
+          To kill the first-born Pharaoh son
+          <br />
+          I&apos;m creeping death
+        </p>
+      </section>
+      <section>
+        <h4>[Verse 2]</h4>
+        <p>
+          Now, let my people go, land of Goshen
+          <br />
+          Go, I will be with thee bush of fire
+          <br />
+          Blood, running red and strong down the Nile
+          <br />
+          Plague, darkness three days long, hail to fire
+        </p>
+      </section>
+      <section>
+        <h4>[Chorus]</h4>
+        <p>
+          So let it be written, so let it be done
+          <br />
+          I&apos;m sent here by the chosen one
+          <br />
+          So let it be written, so let it be done
+          <br />
+          To kill the first-born Pharaoh son
+          <br />
+          I&apos;m creeping death
+        </p>
+      </section>
+      <section>
+        <h4>[Guitar Solo]</h4>
+      </section>
+      <section>
+        <h4>[Instrumental Break]</h4>
+      </section>
+      <section>
+        <h4>[Bridge]</h4>
+        <p>
+          Die by my hand (Die, die)
+          <br />
+          I creep across the land (Die, die)
+          <br />
+          Killing first-born man (Die, die, die)
+          <br />
+          Die by my hand (Die, die)
+          <br />
+          I creep across the land (Die, die)
+          <br />
+          Killing first-born man (Die, die, die)
+        </p>
+      </section>
+      <section>
+        <h4>[Verse 3]</h4>
+        <p>
+          I rule the midnight air, the destroyer
+          <br />
+          Born, I shall soon be there, deadly mass
+          <br />
+          I creep the steps and floor, final darkness
+          <br />
+          Blood, lamb&apos;s blood-painted door I shall pass
+        </p>
+      </section>
+      <section>
+        <h4>[Chorus]</h4>
+        <p>
+          So let it be written, so let it be done
+          <br />
+          I&apos;m sent here by the chosen one
+          <br />
+          So let it be written, so let it be done
+          <br />
+          To kill the first-born Pharaoh son
+          <br />
+          I&apos;m creeping death
+        </p>
+      </section>
+      <section>
+        <h4>[Instrumental Outro]</h4>
       </section>
     </>
   );
