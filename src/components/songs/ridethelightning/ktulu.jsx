f5b56c2be984e65932916f4834602e0694178c13
Fabien LAQUERRIERE <fabienlqr@gmail.com>
2026-03-27 14:57:43 +0100

    SET lyrics of The Call of Ktulu

--- src/components/songs/ridethelightning/ktulu.jsx
+++ src/components/songs/ridethelightning/ktulu.jsx
@@ -1,19 +1,21 @@
 import Player from "../../Player";
 import ktuluMusic from "../../../assets/musics/rideTheLightning/theCallOfKtulu.mp3";
 
-function Ktulu () {
-    return(<>
-          <header className="albumHeader">
-            <h3>The Call Of Ktulu</h3>
-            <p>
-              <time dateTime="PT8M52S">8&apos; 52&quot;</time>
-            </p>
-            <Player music={ktuluMusic} />
-          </header>
-          <section>
-            <h4>[Pas de lyrics, juste profitez de ce solo de bass incroyable!]</h4>
-          </section>
-        </>)
+function Ktulu() {
+  return (
+    <>
+      <header className="albumHeader">
+        <h3>The Call Of Ktulu</h3>
+        <p>
+          <time dateTime="PT8M52S">8&apos; 52&quot;</time>
+        </p>
+        <Player music={ktuluMusic} />
+      </header>
+      <section>
+        <h4>[Pas de lyrics, juste profitez de cette incroyable musique!]</h4>
+      </section>
+    </>
+  );
 }
 
 export default Ktulu;
