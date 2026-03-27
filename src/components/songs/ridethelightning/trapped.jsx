cb807490b0cef5fa9e8c525571ab58fd84c523e4
Fabien LAQUERRIERE <fabienlqr@gmail.com>
2026-03-27 14:32:15 +0100

    SET lyrics of Trapped uder ice

--- src/components/songs/ridethelightning/trapped.jsx
+++ src/components/songs/ridethelightning/trapped.jsx
@@ -12,8 +12,81 @@ function Trapped() {
         <Player music={trappedMusic} />
       </header>
       <section>
-        <h4>[Comes later]</h4>
+        <h4>[Instrumental Intro]</h4>
       </section>
+      <section>
+        <h4>[Verse 1]</h4>
+        <p>I don&apos;t know how to live through this hell<br />
+Woken up, I&apos;m still locked in this shell<br />
+Frozen soul, frozen down to the core<br />
+Break the ice, I can&apos;t take anymore</p>
+      </section>
+      <section>
+        <h4>[Chorus]</h4>
+        <p>Freezing (Freezing), can&apos;t move at all<br />
+Screaming (Screaming), can&apos;t hear my call<br />
+I am dying to live<br />
+Cry out, I&apos;m trapped under ice</p>
+      </section>
+      <section>
+        <h4>[Guitar Solo]</h4>
+      </section>
+      <section>
+        <h4>[Verse 2]</h4>
+        <p>Crystallized as I lay here and rest<br />
+Eyes of glass stare directly at death<br />
+From deep sleep, I have broken away<br />
+No one knows, no one hears what I say</p>
+      </section>
+      <section>
+        <h4>[Chorus]</h4>
+        <p>Freezing (Freezing), can&apos;t move at all<br />
+Screaming (Screaming), can&apos;t hear my call<br />
+I am dying to live<br />
+Cry out, I&apos;m trapped under ice</p>
+      </section>
+      <section>
+        <h4>[Instrumental Break]</h4>
+      </section>
+      <section>
+        <h4>[Bridge]</h4>
+        <p>(Scream) From my soul<br />
+(Fate) Mystified<br />
+(Hell) Forevermore<br />
+(Scream) From my soul<br />
+(Fate) Mystified<br />
+(Hell) Forevermore</p>
+      </section>
+      <section>
+        <h4>[Instrumental Break]</h4>
+      </section>
+      <section>
+        <h4>[Guitar Solo]</h4>
+      </section>
+      <section>
+        <h4>[Verse 3]</h4>
+        <p>No release from my cryonic state<br />
+What is this? I&apos;ve been stricken by fate<br />
+Wrapped up tight, cannot move, can&apos;t break free<br />
+Hand of doom has a tight grip on me</p>
+      </section>
+      <section>
+        <h4>[Chorus]</h4>
+        <p>Freezing (Freezing), can&apos;t move at all<br />
+Screaming (Screaming), can&apos;t hear my call<br />
+I am dying to live<br />
+Cry out, I&apos;m trapped under ice<br />
+Freezing (Freezing), can&apos;t move at all<br />
+Screaming (Screaming), can&apos;t hear my call<br />
+I am dying to live<br />
+Cry out, I&apos;m trapped under ice</p>
+      </section>
+
+
+
+
+
+
     </>
   );
 }
