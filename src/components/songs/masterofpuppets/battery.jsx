77624695837be48d8f52d056bedb6aefd0b045e8
Fabien LAQUERRIERE <fabienlqr@gmail.com>
2026-04-02 16:41:40 +0200

    SET lyrics of Battery

--- src/components/songs/masterofpuppets/battery.jsx
+++ src/components/songs/masterofpuppets/battery.jsx
@@ -12,7 +12,110 @@ function Battery() {
         <Player music={batteryMusic} />
       </header>
       <section>
-        <h4>[Soon]</h4>
+        <h4>[Instrumental Intro]</h4>
+      </section>
+      <section>
+        <h4>[Verse 1]</h4>
+        <p>
+          Lashing out the action, returning the reaction
+          <br />
+          Weak are ripped and torn away
+          <br />
+          Hypnotizing power, crushing all that cower
+          <br />
+          Battery is here to stay
+        </p>
+      </section>
+      <section>
+        <h4>[Chorus]</h4>
+        <p>
+          Smashing through the boundaries, lunacy has found me
+          <br />
+          Cannot stop the battery
+          <br />
+          Pounding out aggression, turns into obsession
+          <br />
+          Cannot kill the battery
+          <br />
+          Cannot kill the family, battery is found in me
+          <br />
+          Battery
+          <br />
+          Battery
+        </p>
+      </section>
+      <section>
+        <h4>[Verse 2]</h4>
+        <p>
+          Crushing all deceivers, mashing non-believers
+          <br />
+          Never-ending potency
+          <br />
+          Hungry violence-seeker, feeding off the weaker
+          <br />
+          Breeding on insanity
+        </p>
+      </section>
+      <section>
+        <h4>[Chorus]</h4>
+        <p>
+          Smashing through the boundaries, lunacy has found me
+          <br />
+          Cannot stop the battery
+          <br />
+          Pounding out aggression, turns into obsession
+          <br />
+          Cannot kill the battery
+          <br />
+          Cannot kill the family, battery is found in me
+          <br />
+          Battery
+          <br />
+          Battery
+        </p>
+      </section>
+      <section>
+        <h4>[Instrumental Break]</h4>
+      </section>
+      <section>
+        <h4>[Guitar Solo]</h4>
+      </section>
+      <section>
+        <h4>[Verse 3]</h4>
+        <p>
+          Circle of destruction, hammer comes crushing
+          <br />
+          Powerhouse of energy
+          <br />
+          Whipping up a fury, dominating flurry
+          <br />
+          We create the battery
+        </p>
+      </section>
+      <section>
+        <h4>[Chorus]</h4>
+        <p>
+          Smashing through the boundaries, lunacy has found me
+          <br />
+          Cannot stop the battery
+          <br />
+          Pounding out aggression, turns into obsession
+          <br />
+          Cannot kill the battery
+          <br />
+          Cannot kill the family, battery is found in me
+          <br />
+          Battery
+          <br />
+          Battery
+          <br />
+          Battery
+          <br />
+          Battery
+        </p>
+      </section>
+      <section>
+        <h4>[Instrumental Outro]</h4>
       </section>
     </>
   );
