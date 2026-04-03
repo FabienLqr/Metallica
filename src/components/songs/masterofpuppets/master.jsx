b017410303fea3920bc3d616812fd56d6461d360
Fabien LAQUERRIERE <fabienlqr@gmail.com>
2026-04-03 12:46:01 +0200

    SET lyrics of Mater of Puppets

--- src/components/songs/masterofpuppets/master.jsx
+++ src/components/songs/masterofpuppets/master.jsx
@@ -7,12 +7,212 @@ function Master() {
       <header className="albumHeader">
         <h3>Master of Puppets</h3>
         <p>
-            <time dateTime="PT8M37S">8&apos; 37&quot;</time>
+          <time dateTime="PT8M37S">8&apos; 37&quot;</time>
         </p>
         <Player music={masterMusic} />
       </header>
       <section>
-        <h4>[Soon]</h4>
+        <h4>[Instrumental Intro]</h4>
+      </section>
+      <section>
+        <h4>[Verse 1]</h4>
+        <p>
+          End of passion play, crumbling away
+          <br />
+          I&apos;m your source of self-destruction
+          <br />
+          Veins that pump with fear, sucking darkest clear
+          <br />
+          Leading on your death&apos;s construction
+        </p>
+      </section>
+      <section>
+        <h4>[Refrain]</h4>
+        <p>
+          Taste me, you will see
+          <br />
+          More is all you need
+          <br />
+          Dedicated to
+          <br />
+          How I&apos;m killing you
+          <br />
+        </p>
+      </section>
+      <section>
+        <h4>[Pre-Chorus]</h4>
+        <p>
+          Come crawling faster
+          <br />
+          Obey your master
+          <br />
+          Your life burns faster
+          <br />
+          Obey your master, master
+        </p>
+      </section>
+      <section>
+        <h4>[Chorus]</h4>
+        <p>
+          Master of puppets, I&apos;m pulling your strings
+          <br />
+          Twisting your mind and smashing your dreams
+          <br />
+          Blinded by me, you can&apos;t see a thing
+          <br />
+          Just call my name &apos;cause I&apos;ll hear you scream
+          <br />
+          Master, master
+          <br />
+          Just call my name &apos;cause I&apos;ll hear you scream
+          <br />
+          Master, master
+        </p>
+      </section>
+      <section>
+        <h4>[Verse 2]</h4>
+        <p>
+          Needlework the way, never you betray
+          <br />
+          Life of death becoming clearer
+          <br />
+          Pain monopoly, ritual misery
+          <br />
+          Chop your breakfast on a mirror
+        </p>
+      </section>
+      <section>
+        <h4>[Refrain]</h4>
+        <p>
+          Taste me, you will see
+          <br />
+          More is all you need
+          <br />
+          Dedicated to
+          <br />
+          How I&apos;m killing you
+        </p>
+      </section>
+      <section>
+        <h4>[Pre-Chorus]</h4>
+        <p>
+          Come crawling faster
+          <br />
+          Obey your master
+          <br />
+          Your life burns faster
+          <br />
+          Obey your master, master
+        </p>
+      </section>
+      <section>
+        <h4>[Chorus]</h4>
+        <p>
+          Master of puppets, I&apos;m pulling your strings
+          <br />
+          Twisting your mind and smashing your dreams
+          <br />
+          Blinded by me, you can&apos;t see a thing
+          <br />
+          Just call my name &apos;cause I&apos;ll hear you scream
+          <br />
+          Master, master
+          <br />
+          Just call my name &apos;cause I&apos;ll hear you scream
+          <br />
+          Master, master (Master, master, master, master)
+        </p>
+      </section>
+      <section>
+        <h4>[Guitar Solo]</h4>
+      </section>
+      <section>
+        <h4>[Instrumental Break]</h4>
+      </section>
+      <section>
+        <h4>[Bridge]</h4>
+        <p>
+          Master, master
+          <br />
+          Where&apos;s the dreams that I&apos;ve been after?
+          <br />
+          Master, master
+          <br />
+          You promised only lies
+          <br />
+          Laughter, laughter
+          <br />
+          All I hear or see is laughter
+          <br />
+          Laughter, laughter
+          <br />
+          Laughing at my cries
+          <br />
+          Fix me
+        </p>
+      </section>
+      <section>
+        <h4>[Guitar Solo]</h4>
+      </section>
+      <section>
+        <h4>[Instrumental Break]</h4>
+      </section>
+      <section>
+        <h4>[Verse 3]</h4>
+        <p>
+          Hell is worth all that, natural habitat
+          <br />
+          Just a rhyme without a reason
+          <br />
+          Never-ending maze, drift on numbered days
+          <br />
+          Now your life is out of season
+        </p>
+      </section>
+      <section>
+        <h4>[Refrain]</h4>
+        <p>
+          I will occupy
+          <br />
+          I will help you die
+          <br />
+          I will run through you
+          <br />
+          Now I rule you too
+        </p>
+      </section>
+      <section>
+        <h4>[Pre-Chorus]</h4>
+        <p>
+          Come crawling faster
+          <br />
+          Obey your master
+          <br />
+          Your life burns faster
+          <br />
+          Obey your master, master
+        </p>
+      </section>
+      <section>
+        <h4>[Chorus]</h4>
+        <p>
+          Master of puppets, I&apos;m pulling your strings
+          <br />
+          Twisting your mind and smashing your dreams
+          <br />
+          Blinded by me, you can&apos;t see a thing
+          <br />
+          Just call my name &apos;cause I&apos;ll hear you scream
+          <br />
+          Master, master
+          <br />
+          Just call my name &apos;cause I&apos;ll hear you scream
+          <br />
+          Master, master
+        </p>
+      </section>
+      <section>
+        <h4>[Outro]</h4>
       </section>
     </>
   );
